43c5096a45787f6b0288fbc4646c65f1bc85e2ef
Taiga Saito <me@taigasaito.org>
2026-05-29 12:14:54 +0900

    feat: JSON LD, sitemap など

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: "https://taigasaito.org",
+  integrations: [sitemap({ filter: (page) => !page.includes("/404") })],
   vite: {
     plugins: [tailwindcss()],
   },
